bbbac58b7186741695b9ff09e9282e9e8e4af1e9
aronwebsolution <dev@aronwebsolutions.com>
2026-09-09 12:07:32 +0530

    p

--- Employees.UI/Program.cs
+++ Employees.UI/Program.cs
@@ -7,10 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
 //builder.Services.AddScoped<HttpClientWrapper>();
 builder.Services.AddHttpClient<HttpClientWrapper>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7045/");
+    client.BaseAddress = new Uri("https://localhost:44347/");
 });
 
 var app = builder.Build();
