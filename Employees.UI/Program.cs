a08646ba44e78cdf533039c128585d5d2188ae73
aronwebsolution <dev@aronwebsolutions.com>
2026-09-10 10:18:00 +0530

    Update  employeeUi

--- Employees.UI/Program.cs
+++ Employees.UI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorComponents()
 //builder.Services.AddScoped<HttpClientWrapper>();
 builder.Services.AddHttpClient<HttpClientWrapper>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:44347/");
+    client.BaseAddress = new Uri("https://localhost:44397/");
 });
 
 var app = builder.Build();
