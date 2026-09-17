a3e4e6c90c6993ed2c6c62066ca3c995bde62306
pawanpreet singh <pawanpreet1901266@gmail.com>
2026-09-18 00:31:25 +0530

    login and logout page

--- Employees.UI/Components/Pages/Logout.razor.cs
+++ Employees.UI/Components/Pages/Logout.razor.cs
@@ -1,9 +1,16 @@
+using Employees.UI.Services;
+using Microsoft.AspNetCore.Components;
+
 namespace Employees.UI.Components.Pages
 {
     public partial class Logout
     {
+        [Inject]
+        public UserSession Session { get; set; }
+        UserSession session;
         protected override void OnInitialized()
         {
+            Session.Logout();
             Navigation.NavigateTo("/login");
         }
     }
